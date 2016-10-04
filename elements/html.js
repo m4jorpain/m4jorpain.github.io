3f63056aa04f78b733309ad6f5bd8a5677820961
Caspar0v <cbvanbeusekom@12move.nl>
2016-10-04 13:51:25 +0200

    dune

--- elements/html.js
+++ elements/html.js
@@ -40,9 +40,9 @@ var updateUpgrades = function(){
 		&& (alreadyUpgradeId(player.upgradeList[i].requiredUpgrade) || player.upgradeList[i].requiredUpgrade == null )){
 			var upgrade = player.upgradeList[i];
 			if(player.money > upgrade.cost){
-				$("#upgradeBox").append("<button type=button id=Upgrade"+upgrade.id+" title=s class='upgradeBoxes btn btn-primary col-sm-12'>"+upgrade.name+"<br>Cost: "+upgrade.cost+"<p id=flavor> </p></button>");
+				$("#upgradeBox").append("<button type=button id=Upgrade"+upgrade.id+" class='upgradeBoxes btn btn-primary col-sm-12'>"+upgrade.name+"<br>Cost: "+upgrade.cost+"<p id=flavor> </p></button>");
 			} else {
-				$("#upgradeBox").append("<button type=button id=Upgrade"+upgrade.id+" title=s class='upgradeBoxes disabled btn btn-primary col-sm-12'>"+upgrade.name+"<br>Cost: "+upgrade.cost+"<p id=flavor> </p></button>");
+				$("#upgradeBox").append("<button type=button id=Upgrade"+upgrade.id+" class='upgradeBoxes disabled btn btn-primary col-sm-12'>"+upgrade.name+"<br>Cost: "+upgrade.cost+"<p id=flavor> </p></button>");
 			}
 			document.getElementById("flavor").innerHTML = upgrade.flavorText;
 		}
